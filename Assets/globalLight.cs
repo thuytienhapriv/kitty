b375bd9074e0af019afde4a1a153fbbc2378333f
kaisaedrew <kaisaedrew@gmail.com>
2024-04-17 13:56:03 +0200

    Jump Blender file

--- Assets/globalLight.cs
+++ Assets/globalLight.cs
@@ -32,12 +32,13 @@ public class globalLight : MonoBehaviour
     }
     public void day()
     {
-        sun.color = new Color(0f, 0.3f, 0.2f);
+        sun.color += new Color(.2f, .2f, .2f);
+        sun.color = new Color(1f, 1f, 1f);
         dayTime = true;
     } 
     public void night()
     {
-        sun.color = new Color(0.4f, 0f, 0.2f);
+        sun.color = new Color(0.5f, 0.4f, 0.8f);
         dayTime = false;
     }
 }
